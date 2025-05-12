d0261e69bd8af5019ebe135c15b543e9bd34dd2a
Florian BRUN <flo33.brun@hotmail.fr>
2025-05-12 13:03:54 +0200

    integration of drop-down menu

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Bonjour");
+let sideBar = document.getElementById("side-bar");
+console.log(sideBar);
+
+let burgerButton = document.querySelector(".burger");
+console.log(burgerButton);
+
+let searchField = document.getElementById("search-field");
+burgerButton.addEventListener("click", () => {
+  console.log("click");
+  sideBar.classList.toggle("active");
+});
