1b5eea6f346b27d84c16255c5f27ed45c91f3b6b
Florian BRUN <flo33.brun@hotmail.fr>
2025-05-16 21:51:21 +0200

    Added fetch call to load data from backend API

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+fetch("http://localhost:3000/api/v1/jokes")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Erreur:", error));
+
 let sideBar = document.getElementById("side-bar");
 console.log(sideBar);
 
 let burgerButton = document.querySelector(".burger");
 console.log(burgerButton);
 
-let searchField = document.getElementById("search-field")
+let searchField = document.getElementById("search-field");
 burgerButton.addEventListener("click", () => {
   console.log("click");
   sideBar.classList.toggle("active");
